02e529b3ed265a06d8306d59832f93b9265ee7c6
Aryan <aryan.shrivastva19@gmail.com>
2025-04-03 02:36:22 +0530

    Q1 completed

--- Unit_Converter/app/src/main/java/com/example/unit_converter/MainActivity.java
+++ Unit_Converter/app/src/main/java/com/example/unit_converter/MainActivity.java
@@ -1,36 +1,34 @@
 package com.example.unit_converter;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import java.text.DecimalFormat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
-private Spinner fromUnitSpinner;
-private Spinner toUnitSpinner;
-private EditText editTextNumber;
-private TextView textView4;
-private boolean isUpdating = false;
-private DecimalFormat df = new DecimalFormat("#.####");
+    private Spinner fromUnitSpinner;
+    private Spinner toUnitSpinner;
+    private EditText editTextNumber;
+    private TextView textView4;
+    private Button convertButton;
+    private DecimalFormat df = new DecimalFormat("#.####");
 
     private final double feet_to_meter = 0.3048;
     private final double inch_to_meter = 0.0254;
     private final double cm_to_meter = 0.01;
     private final double yards_to_meter = 0.9144;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,33 +40,115 @@ private DecimalFormat df = new DecimalFormat("#.####");
         textView4 = findViewById(R.id.textView4);
         fromUnitSpinner = findViewById(R.id.fromUnit);
         toUnitSpinner = findViewById(R.id.toUnit);
+        convertButton = findViewById(R.id.button);
 
+        // Creating adapter for both spinners
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
                 this,
                 R.array.unitConverter,
                 android.R.layout.simple_spinner_item
         );
 
+
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
+        //adapter for both spinners
         fromUnitSpinner.setAdapter(adapter);
+        toUnitSpinner.setAdapter(adapter);
 
+        //listeners for both spinners
+        fromUnitSpinner.setOnItemSelectedListener(this);
+        toUnitSpinner.setOnItemSelectedListener(this);
 
-        public class SpinnerActivity extends Activity implements AdapterView.OnItemSelectedListener{
-            public void onItemSelected(AdapterView<?> parent, View view, int pos, long id){
+        //default selections
+        fromUnitSpinner.setSelection(0); // Set to first item, Feet
+        toUnitSpinner.setSelection(1); // Set to second item, Inches
 
+        //default value in TextView
+        textView4.setText("0");
+
+        //click listener to convert button
+        convertButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                convertUnits();
             }
-            public void onNothingSelected(AdapterView<?> parent){}
+        });
+    }
 
-
-        }
-
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-//            return insets;
-//        });
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
     }
 
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+    private void convertUnits() {
+        //input values
+        String inputString = editTextNumber.getText().toString();
+        if (inputString.isEmpty()) {
+            textView4.setText("0");
+            return;
+        }
+
+        try {
+            double value = Double.parseDouble(inputString);
+            String fromUnit = fromUnitSpinner.getSelectedItem().toString();
+            String toUnit = toUnitSpinner.getSelectedItem().toString();
+
+            double result = convert(value, fromUnit, toUnit);
+            textView4.setText(df.format(result));
+        } catch (NumberFormatException e) {
+            textView4.setText("Invalid input");
+        }
+    }
+
+    private double convert(double value, String fromUnit, String toUnit) {
+        // Converting unit to meters
+        double valueInMeters = 0;
+
+        switch (fromUnit) {
+            case "Feet":
+                valueInMeters = value * feet_to_meter;
+                break;
+            case "Inches":
+                valueInMeters = value * inch_to_meter;
+                break;
+            case "Centimeters":
+                valueInMeters = value * cm_to_meter;
+                break;
+            case "Meters":
+                valueInMeters = value;
+                break;
+            case "Yards":
+                valueInMeters = value * yards_to_meter;
+                break;
+        }
+
+        // Converting from meters to unit
+        double result = 0;
+
+        switch (toUnit) {
+            case "Feet":
+                result = valueInMeters / feet_to_meter;
+                break;
+            case "Inches":
+                result = valueInMeters / inch_to_meter;
+                break;
+            case "Centimeters":
+                result = valueInMeters / cm_to_meter;
+                break;
+            case "Meters":
+                result = valueInMeters;
+                break;
+            case "Yards":
+                result = valueInMeters / yards_to_meter;
+                break;
+        }
+
+        return result;
+    }
 }
